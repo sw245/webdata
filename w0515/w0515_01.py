fb641e7ba0c9336ecb2f4e0f87e3e79007614fdc
sw245 <qtd2012@gmail.com>
2025-05-16 18:31:19 +0900

    friday

--- w0515/w0515_01.py
+++ w0515/w0515_01.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
---
+++ w0516/p0516.py
@@ -0,0 +1,9 @@
+# import csv
+
+# with open('files/stock1.csv','r',encoding='utf-8-sig') as f:
+#     f.
+
+import pandas as pd
+
+df = pd.read_csv('w0516/files/stock1.csv')
+print(df)
